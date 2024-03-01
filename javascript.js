6c9945c04dac8aa7f671cf12f951cdb3c484be8e
AlisterProfitt <alisterprofitt@gmail.com>
2024-03-01 14:48:28 -0400

    Add game logic, play for one round

--- javascript.js
+++ javascript.js
@@ -3,13 +3,46 @@ function getComputerChoice() {
     console.log(diceRoll)
     let computerChoice;
     if (diceRoll < 0.33) {
-        computerChoice = 'Rock'
+        computerChoice = 'rock'
     } else if (diceRoll < 0.66) {
-        computerChoice = 'Paper'
+        computerChoice = 'paper'
     } else if (diceRoll < 1) {
-        computerChoice = 'Scissors'
+        computerChoice = 'scissors'
     };
     return computerChoice;
 }
 
 console.log(getComputerChoice());
+
+function oneRoundGame(playerSelection, computerSelection) {
+    let result;
+    if (playerSelection.toLowerCase() == 'rock') {
+        if (computerSelection.toLowerCase() == 'rock') {
+            result = 'Tied!';
+        } else if (computerSelection.toLowerCase() == 'paper') {
+            result = 'Lost!';
+        } else if (computerSelection.toLowerCase() == 'scissors') {
+            result = 'Won!';
+        }
+    } else if (playerSelection.toLowerCase() == 'paper') {
+        if (computerSelection.toLowerCase() == 'rock') {
+            result = 'Won!';
+        } else if (computerSelection.toLowerCase() == 'paper') {
+            result = 'Tied!';
+        } else if (computerSelection.toLowerCase() === 'scissors') {
+            result = 'Lost!';
+        }
+    } else if (playerSelection.toLowerCase() == 'scissors') {
+        if (computerSelection.toLowerCase() == 'rock') {
+            result = 'Lost!';
+        } else if (computerSelection.toLowerCase() === 'paper') {
+            result = 'Won!';
+        } else if (computerSelection.toLowerCase() == 'scissors') {
+            result = 'Tied!';
+        }
+    }
+    return result;
+}
+
+console.log(oneRoundGame('ScIsSOrS', 'rOCk'));
+
