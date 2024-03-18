00967b619679a2ff4421da6e291bec1b9d38de16
AlisterProfitt <alisterprofitt@gmail.com>
2024-03-18 15:11:10 -0400

    add basic UI to RPS

--- javascript.js
+++ javascript.js
@@ -11,45 +11,99 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function oneRoundGame(playerSelection, computerSelection) {
+let playerScore = 0,
+    computerScore = 0
+
+function rockButton(computerSelection) {
+    computerSelection = getComputerChoice();
     let result;
-    if (playerSelection.toLowerCase() == 'rock') {
-        if (computerSelection.toLowerCase() == 'rock') {
-            result = 'Game Tied! Computer also chose Rock!';
-        } else if (computerSelection.toLowerCase() == 'paper') {
-            result = 'You Lose! Computer chose Paper, and Paper covers Rock!';
-        } else if (computerSelection.toLowerCase() == 'scissors') {
-            result = 'You Win! Computer chose Scissors, and Rock breaks Scissors!';
-        }
-    } else if (playerSelection.toLowerCase() == 'paper') {
-        if (computerSelection.toLowerCase() == 'rock') {
-            result = 'You Win! Computer chose Rock, and Paper covers Rock!';
-        } else if (computerSelection.toLowerCase() == 'paper') {
-            result = 'Game Tied! Computer also chose Paper!';
-        } else if (computerSelection.toLowerCase() == 'scissors') {
-            result = 'You Lose! Computer chose Scissors, and Scissors cuts Paper!';
-        }
-    } else if (playerSelection.toLowerCase() == 'scissors') {
-        if (computerSelection.toLowerCase() == 'rock') {
-            result = 'You Lose! Computer chose Rock, and Rock breaks Scissors!';
-        } else if (computerSelection.toLowerCase() == 'paper') {
-            result = 'You Win! Computer chose Paper, and Scissors cuts Paper!';
-        } else if (computerSelection.toLowerCase() == 'scissors') {
-            result = 'Tied! Computer also chose Scissors!';
-        }
+    if (computerSelection == 'rock') {
+        result = 'Game Tied! Computer also chose Rock!';
+    } else if (computerSelection == 'paper') {
+        result = 'You Lose! Computer chose Paper, and Paper covers Rock!';
+        computerScore++;
+    } else if (computerSelection == 'scissors') {
+        result = 'You Win! Computer chose Scissors, and Rock breaks Scissors!';
+        playerScore++;
     }
-    return result;
-}
-
-function playGame() {
-    let rounds = parseInt(prompt('How many rounds of Rock Paper Scissors would you like to play?', ''));
-    for (let i = 0; i<rounds; i++) {
-    let playerSelection = prompt('What is your choice? Rock, Paper, or Scissors?', '');
-    let computerSelection = getComputerChoice();
-    result = oneRoundGame(playerSelection, computerSelection);
-    alert(result);
-    console.log(result);
+    results.textContent = result;
+    score.textContent = `Score: You ${playerScore} - ${computerScore} Computer`;
+    if (playerScore >= 5) {
+        alert('Game Finished! You have won!!!');
+        rock.removeEventListener('click', rockButton);
+        paper.removeEventListener('click', paperButton);
+        scissors.removeEventListener('click', scissorsButton);
+    } else if (computerScore >=5) {
+        alert('Game Finished! Computer has won!!!')
+        rock.removeEventListener('click', rockButton);
+        paper.removeEventListener('click', paperButton);
+        scissors.removeEventListener('click', scissorsButton);
     }
 }
 
-playGame();
+function paperButton(computerSelection) {
+    computerSelection = getComputerChoice();
+    let result;
+    if (computerSelection.toLowerCase() == 'rock') {
+        result = 'You Win! Computer chose Rock, and Paper covers Rock!';
+        playerScore++;
+    } else if (computerSelection.toLowerCase() == 'paper') {
+        result = 'Game Tied! Computer also chose Paper!';
+    } else if (computerSelection.toLowerCase() == 'scissors') {
+        result = 'You Lose! Computer chose Scissors, and Scissors cuts Paper!';
+        computerScore++;
+    }
+    results.textContent = result;
+    score.textContent = `Score: You ${playerScore} - ${computerScore} Computer`;
+
+    if (playerScore >= 5) {
+        alert('Game Finished! You have won!!!');
+        rock.removeEventListener('click', rockButton);
+        paper.removeEventListener('click', paperButton);
+        scissors.removeEventListener('click', scissorsButton);
+    } else if (computerScore >=5) {
+        alert('Game Finished! Computer has won!!!')
+        rock.removeEventListener('click', rockButton);
+        paper.removeEventListener('click', paperButton);
+        scissors.removeEventListener('click', scissorsButton);
+    }
+}
+
+function scissorsButton(computerSelection) {
+    computerSelection = getComputerChoice();
+    let result;
+    if (computerSelection.toLowerCase() == 'rock') {
+        result = 'You Lose! Computer chose Rock, and Rock breaks Scissors!';
+        computerScore++
+    } else if (computerSelection.toLowerCase() == 'paper') {
+        result = 'You Win! Computer chose Paper, and Scissors cuts Paper!';
+        playerScore++
+    } else if (computerSelection.toLowerCase() == 'scissors') {
+        result = 'Tied! Computer also chose Scissors!';
+    }
+    results.textContent = result;
+    score.textContent = `Score: You ${playerScore} - ${computerScore} Computer`;
+
+    if (playerScore >= 5) {
+        alert('Game Finished! You have won!!!');
+        rock.removeEventListener('click', rockButton);
+        paper.removeEventListener('click', paperButton);
+        scissors.removeEventListener('click', scissorsButton);
+    } else if (computerScore >=5) {
+        alert('Game Finished! Computer has won!!!')
+        rock.removeEventListener('click', rockButton);
+        paper.removeEventListener('click', paperButton);
+        scissors.removeEventListener('click', scissorsButton);
+    }
+}
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const results = document.querySelector('#result');
+const score = document.querySelector('#score');
+score.textContent = `Score: You ${playerScore} - ${computerScore} Computer`
+
+rock.addEventListener('click', rockButton);
+paper.addEventListener('click', paperButton);
+scissors.addEventListener('click', scissorsButton);
